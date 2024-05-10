871daf5a603f6cccedfd19597d1d7cc563e1d948
leesangjun <prokjun@gmail.com>
2024-05-10 21:34:23 +0900

    잘못된저장이슈 있으나 화면구현은 가능

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -30,63 +30,162 @@ class _MyHomePageState extends State<MyHomePage> {
   bool _isLiked = false;
   TextEditingController _memoController = TextEditingController();
   CalendarFormat _calendarFormat = CalendarFormat.month;
-  late DateTime _focusedDay;
+  DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
-  Map<DateTime, bool> watchedDays = {};
-  SharedPreferences? prefs;
+  Map<String, bool> likedDays = {};
+  Map<String, String> memoDays = {};
 
   @override
   void initState() {
     super.initState();
-    DateTime now = DateTime.now();
-    _focusedDay = DateTime(now.year, now.month, now.day);
-    _selectedDay = _focusedDay;
     _controller = YoutubePlayerController(
       initialVideoId: 'LyCelsH_9L0',
       flags: YoutubePlayerFlags(autoPlay: false),
     );
-    initPrefs();
-    _loadWatchedStatus();
+    _loadPreferences(); // 초기 설정에서 전체 데이터 로드
   }
 
-  void initPrefs() async {
-    prefs = await SharedPreferences.getInstance();
+  void _loadPreferences() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      likedDays = Map<String, bool>.fromIterable(
+          prefs.getKeys().where((key) => key.endsWith('_liked')),
+          key: (item) => item as String,
+          value: (item) => prefs.getBool(item) ?? false);
+      memoDays = Map<String, String>.fromIterable(
+          prefs.getKeys().where((key) => key.endsWith('_memo')),
+          key: (item) => item as String,
+          value: (item) => prefs.getString(item) ?? '');
+    });
   }
 
   void _savePreferences() async {
-    if (prefs != null && _selectedDay != null) {
-      String formattedDate = DateFormat('yyyyMMdd').format(_selectedDay!);
-      await prefs!.setBool(formattedDate, _isLiked);
-      await prefs!.setString(formattedDate + '_memo', _memoController.text);
-      watchedDays[_selectedDay!] = _isLiked;
-      print('Saved: $formattedDate, isLiked: $_isLiked');
-      setState(() {});
-    }
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String formattedDate =
+        DateFormat('yyyyMMdd').format(_selectedDay ?? DateTime.now());
+    await prefs.setBool(formattedDate + '_liked', _isLiked);
+    await prefs.setString(formattedDate + '_memo', _memoController.text);
   }
 
-  void _loadWatchedStatus() async {
-    if (prefs != null) {
-      Set<String> keys = prefs!.getKeys();
-      watchedDays.clear();
-      for (String key in keys) {
-        if (key.endsWith('_memo')) continue;
-        DateTime date = DateFormat('yyyyMMdd').parse(key);
-        bool watched = prefs!.getBool(key) ?? false;
-        watchedDays[date] = watched;
-        print('Loaded: $key, watched: $watched, Date: $date');
-      }
-      setState(() {});
-    }
+  void _updateDataForSelectedDay(DateTime selectedDay) {
+    String keyDate = DateFormat('yyyyMMdd').format(selectedDay);
+    setState(() {
+      _isLiked = likedDays[keyDate + '_liked'] ?? false;
+      _memoController.text = memoDays[keyDate + '_memo'] ?? '';
+    });
+  }
+
+  void _saveTodayPreferences(String keyDate, bool isLiked, String memo) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(keyDate + '_liked', isLiked);
+    await prefs.setString(keyDate + '_memo', memo);
+  }
+
+  Widget buildCalendarTab() {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          TableCalendar(
+            firstDay: DateTime.utc(2010, 10, 16),
+            lastDay: DateTime.utc(2030, 3, 14),
+            focusedDay: _focusedDay,
+            calendarFormat: _calendarFormat,
+            selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+            onDaySelected: (selectedDay, focusedDay) {
+              setState(() {
+                _selectedDay = selectedDay;
+                _focusedDay = focusedDay; // Update `_focusedDay` here as well
+              });
+              _updateDataForSelectedDay(selectedDay);
+            },
+          ),
+          if (_selectedDay != null) // 선택된 날짜가 있을 경우에만 표시
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                DateFormat('yyyy년 M월 d일').format(_selectedDay!),
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ),
+          IconButton(
+            icon: Icon(_isLiked ? Icons.favorite : Icons.favorite_border,
+                size: 48),
+            color: Colors.red,
+            onPressed: () {
+              setState(() {
+                _isLiked = !_isLiked;
+              });
+              _savePreferences();
+            },
+          ),
+          TextField(
+            controller: _memoController,
+            decoration:
+                InputDecoration(labelText: '메모', border: OutlineInputBorder()),
+            maxLines: 5,
+            onChanged: (text) {
+              _savePreferences();
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildTodayTab() {
+    DateTime today = DateTime.now();
+    String todayKey = DateFormat('yyyyMMdd').format(today);
+
+    // '오늘' 탭을 위한 상태 업데이트
+    bool todayIsLiked = likedDays[todayKey + '_liked'] ?? false;
+    String todayMemo = memoDays[todayKey + '_memo'] ?? '';
+
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            DateFormat('yyyy년 M월 d일').format(today),
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+        ),
+        IconButton(
+          icon: Icon(todayIsLiked ? Icons.favorite : Icons.favorite_border,
+              size: 48),
+          color: Colors.red,
+          onPressed: () {
+            setState(() {
+              likedDays[todayKey + '_liked'] = !todayIsLiked;
+              _saveTodayPreferences(todayKey, !todayIsLiked, todayMemo);
+            });
+          },
+        ),
+        TextField(
+          controller: TextEditingController(text: todayMemo),
+          decoration:
+              InputDecoration(labelText: '메모', border: OutlineInputBorder()),
+          maxLines: 5,
+          onChanged: (text) {
+            _saveTodayPreferences(todayKey, todayIsLiked, text);
+          },
+        ),
+      ],
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    DateTime today = DateTime.now();
+    String todayKey = DateFormat('yyyyMMdd').format(today);
+
     return Scaffold(
-      appBar: AppBar(title: Text('Flutter App')),
+      appBar: AppBar(title: Text('플러터 앱')),
       body: IndexedStack(
         index: _selectedIndex,
         children: [
-          // Add other widgets as needed
+          buildCalendarTab(), // '달력' 탭 UI를 메서드 호출로 대체
+          buildTodayTab(), // '오늘' 탭 UI 구성 메서드를 호출 (정의 필요)
+          // 다른 탭의 위젯 구성
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
